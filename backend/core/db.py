490d5de75bdbdf7adf7b9fffba26d0927970f456
Darsh Rawat <rawatdarsh@gmail.com>
2026-02-02 10:26:23 +0530

    Added New Dataset + Fixes in db populate function

--- backend/core/db.py
+++ backend/core/db.py
@@ -12,19 +12,19 @@ collection = client.get_or_create_collection(
 
 # Populate the collection if not populated
 def populate_collection(dataset_path):
-    if collection.count() == 0 : 
-        data = pd.read_csv(f"{dataset_path}")
 
-        titles = data["title"].tolist()
-        ids = data["video_id"].tolist()
-        
-        collection.add(
-            ids=ids,
-            documents=titles,
-        )
-        print("Successfully Populated DB!")
-    else : 
-        print("DB Already Populated !")
+    data = pd.read_csv(f"{dataset_path}")
+
+    titles = data["title"].tolist()
+    ids = data["video_id"].tolist()
+    
+    collection.add(
+        ids=ids,
+        documents=titles,
+    )
+    print("Successfully Populated DB!")
+
 
 if __name__ == "__main__" : 
     populate_collection(r"D:\Code\video-recommendation-system\backend\data\dataset1.csv")
+    populate_collection(r"D:\Code\video-recommendation-system\backend\data\dataset2.csv")
