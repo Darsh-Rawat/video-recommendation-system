1ad7052c1119b1597599a441a52be46acfb7d209
Darsh Rawat <rawatdarsh@gmail.com>
2026-02-08 15:04:21 +0530

    Show actual video details on UI

--- frontend/src/components/VideoCard.jsx
+++ frontend/src/components/VideoCard.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 
 const VideoCard = ({ video }) => {
-    const thumbnailUrl = `https://img.youtube.com/vi/${video.id}/0.jpg`;
+    const thumbnailUrl = `https://img.youtube.com/vi/${video.video_id}/0.jpg`;
 
     const handelWatch = async() => {
     await axios.post("http://127.0.0.1:8000/watch/", {
@@ -19,10 +19,10 @@ return (
         <img src={thumbnailUrl} alt="test" className="thumbnail w-sm rounded-2xl" />
         <div className='right'>
             <h2 className='title font-bold'>{video.title}</h2>
-            <p>6.4M views . 2 days ago</p>
+            <p>{video.views} views . {video.published_at} </p>
             <div className='channel-info w-fit flex gap-2 mt-2 hover:cursor-pointer' onClick={() => { alert("Channel Info") }}>
                 <div className='logo rounded-4xl bg-gray-700 w-7 h-7'><img src={logo} alt="logo" className='size-7' /></div>
-                <p className='channel-name'>Channel name</p>
+                <p className=' font-semibold'>{video.channel_name}</p>
             </div>
             <button className='border-2 p-1 w-20 mt-2 rounded-2xl hover:bg-gray-200 hover:cursor-pointer' onClick={() => { handelWatch() }}>Watch</button>
         </div>
